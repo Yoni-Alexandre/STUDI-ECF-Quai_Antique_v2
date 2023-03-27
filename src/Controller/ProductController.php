3974310fded5383da0d9362f55b8edee2bacb345
Yoni-Alexandre <yoni-brault@blanche-de-castille.fr>
2023-03-27 12:51:28 +0200

    - Route vers produits/categories + templates avec "cards" Bootstrap.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Categories;
 use App\Entity\OpeningHours;
 use App\Entity\Products;
+use App\Repository\CategoriesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,19 +25,32 @@ class ProductController extends AbstractController
         $openingHours = $this->entityManager->getRepository(OpeningHours::class)->findAll();
         $products = $this->entityManager->getRepository(Products::class)->findAll();
 
-
         return $this->render('product/index.html.twig', [
             'openingHours' => $openingHours,
             'products' => $products
         ]);
     }
 
+    #[Route('/produits/categories', name: 'product_categories')]
+    public function productCategory(CategoriesRepository $categoriesRepository): Response
+    {
+        $openingHours = $this->entityManager->getRepository(OpeningHours::class)->findAll();
+
+
+        return $this->render('product/productCategory.html.twig', [
+            'openingHours' => $openingHours,
+            'categories' => $categoriesRepository->findAll()
+        ]);
+    }
+
+
     #[Route('/produit/{slug}', name: 'product')]
-    public function showSlug($slug): Response
+    public function showSlug(Products $productSlug/*, $id */ ): Response
     {
 
         $openingHours = $this->entityManager->getRepository(OpeningHours::class)->findAll();
-        $productSlug = $this->entityManager->getRepository(Products::class)->findOneBySlug($slug);
+//        $productSlug = $this->entityManager->getRepository(Products::class)->findOneBySlug($slug);
+
 
         if (!$productSlug) {
             return $this->redirectToRoute('products');
@@ -46,4 +61,5 @@ class ProductController extends AbstractController
             'productSlug' => $productSlug
         ]);
     }
+
 }
